1a46f4d97ad36f935a806f0933b12fcaaa997098
shivamksinha11 <shivamksinha11@gmail.com>
2022-05-14 21:24:34 +0530

    weather-app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '27803e722bmsh0dcfc1d7405af79p13b0b1jsn27ea361eaaae'
 };
 
 /*
