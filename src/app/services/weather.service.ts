0901703779040d198b346203e79154fd9f5fe643
shivamksinha11 <shivamksinha11@gmail.com>
2022-05-14 21:29:05 +0530

    weather-app

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,10 +12,10 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(cityName: string): Observable<WeatherData> {
-    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+    return this.http.get<WeatherData>('https://community-open-weather-map.p.rapidapi.com/weather', {
       headers: new HttpHeaders()
-        .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
-        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+        .set('X-RapidAPI-Host', 'community-open-weather-map.p.rapidapi.com')
+        .set('X-RapidAPI-Key', '27803e722bmsh0dcfc1d7405af79p13b0b1jsn27ea361eaaae'),
       params: new HttpParams()
         .set('q', cityName)
         .set('units', 'metric')
